191d02c506da4820e486de98868375f21d0af513
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-04-04 12:54:42 +0300

    error correction

--- home_work/050/Program.cs
+++ home_work/050/Program.cs
@@ -22,18 +22,10 @@ void PrintArray(int[,] coll)
     }
 }
 
-void CheckElement(int[,] coll, int a)
+void CheckElement(int[,] coll, int a, int b)
 {
-    bool flag = false;
-    for (int i = 0; i < coll.GetLength(0); i++)
-    {
-        for (int j = 0; j < coll.GetLength(1); j++)
-        {
-            if (a == coll[i, j]) flag = true;
-        }
-    }
-    if (flag == true) Console.WriteLine($"Number {a} is in the array!");
-    else Console.WriteLine("No such number exists in the array.");
+    if (a < coll.GetLength(0) && b < coll.GetLength(1)) Console.WriteLine($"Element {coll[a, b]} is in the array!");
+    else Console.WriteLine("No such element exists in the array.");
 }
 
 Console.Write("Rows number: ");
@@ -43,6 +35,8 @@ int n = Convert.ToInt32(Console.ReadLine());
 int[,] matrix = new int[m, n];
 FillArray(matrix);
 PrintArray(matrix);
-Console.Write("\nEnter the number: ");
+Console.Write("\nEnter the row number: ");
 int b = Convert.ToInt32(Console.ReadLine());
-CheckElement(matrix, b);
+Console.Write("Enter the column number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+CheckElement(matrix, b, c);
--- home_work/052/Program.cs
+++ home_work/052/Program.cs
@@ -1,4 +1,4 @@
-﻿void FillArray(int[,] coll)
+﻿void FillArray(double[,] coll)
 {
     Random rnd = new Random();
     for (int i = 0; i < coll.GetLength(0); i++)
@@ -10,22 +10,22 @@
     }
 }
 
-void PrintArray(int[,] coll)
+void PrintArray(double[,] coll)
 {
     for (int i = 0; i < coll.GetLength(0); i++)
     {
         for (int j = 0; j < coll.GetLength(1); j++)
         {
-            Console.Write($"{coll[i, j], -3}");
+            Console.Write($"{coll[i, j], -5}");
         }
         Console.WriteLine();
     }
 }
 
-void Mean(int[,] coll)
+void Mean(double[,] coll)
 {
     int i = 0;
-    int mean = 0;
+    double mean = 0;
     int columns = 1;
     for (int j = 0; j < coll.GetLength(1); j++)
     {
@@ -34,7 +34,7 @@ void Mean(int[,] coll)
             mean += coll[i, j];
             i++;
         }
-        Console.WriteLine($"{columns} column's mean = {mean}");
+        Console.WriteLine($"{columns} column's mean = {mean / coll.GetLength(0):f1}");
         columns++;
         mean = 0;
         i = 0;
@@ -45,7 +45,7 @@ Console.Write("Rows number: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Columns number: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = new int[m, n];
+double[,] matrix = new double[m, n];
 FillArray(matrix);
 PrintArray(matrix);
 Console.WriteLine();
